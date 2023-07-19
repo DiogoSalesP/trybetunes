8bbc19c3f8f2272e013a370a1307e94eafbcb213
Diogo Sales <diogosales.ads@gmail.com>
2023-07-19 15:37:07 -0300

    feat: form to search artists

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,10 +1,41 @@
+import { useState } from 'react';
 import Header from '../../components/header';
 
 export default function Search() {
+  const [search, setSearch] = useState('');
+  const [disble, setDisable] = useState(true);
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const { value } = event.target;
+    if (value.length >= 2) {
+      setDisable(false);
+    } else {
+      setDisable(true);
+    }
+    setSearch(value);
+  }
+
   return (
     <>
       <Header />
-      <h1>Search</h1>
+      <form>
+        <label htmlFor="search-artist">
+          <input
+            type="text"
+            id="search-artist"
+            name="search-artist"
+            value={ search }
+            onChange={ handleChange }
+            placeholder="Nome do artista"
+          />
+          <button
+            type="button"
+            disabled={ disble }
+          >
+            Procurar
+          </button>
+        </label>
+      </form>
     </>
   );
 }
