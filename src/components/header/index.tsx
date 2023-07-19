9a66f02271b672366c726803d55f85c7baf985b7
Diogo Sales <diogosales.ads@gmail.com>
2023-07-19 13:30:38 -0300

    feat: header navigation links

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,22 +1,22 @@
 import { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import { getUser } from '../../service/userAPI';
 import { CreateUserType } from '../../types';
 
 export default function Header() {
   const [user, setUser] = useState<CreateUserType>();
 
-  function getLocalstorage() {
-    const data = getUser();
-    return data;
-  }
-
   useEffect(() => {
-    setUser(getLocalstorage());
+    setUser(getUser());
   }, []);
 
   return (
     <header>
-      <p>Nome</p>
+      <div>
+        <NavLink to="/search">Pesquisar</NavLink>
+        <NavLink to="/favorites">Músicas favoritas</NavLink>
+        <NavLink to="/profile">Perfil</NavLink>
+      </div>
       <p>{user?.name}</p>
     </header>
   );
