c38eb40ffa3f52a338746db0867043d2bb12793a
Diogo Sales <diogosales.ads@gmail.com>
2023-07-20 11:02:06 -0300

    feat: request to search artists

---
+++ src/service/searchAlbumAPI.ts
@@ -0,0 +1,34 @@
+import { AlbumsType } from '../types';
+
+export default async function searchAlbumsAPI(artist: string) {
+  const artistNameURL = encodeURI(artist).replaceAll('%20', '+');
+
+  const getAlbumsAPI = `https://itunes.apple.com/search?entity=album&term=${artistNameURL}&attribute=allArtistTerm`;
+
+  const APIResponse = await fetch(getAlbumsAPI);
+
+  const { results } = await APIResponse.json();
+
+  const response = results.map(
+    ({
+      artistId,
+      artistName,
+      collectionId,
+      collectionName,
+      collectionPrice,
+      artworkUrl100,
+      releaseDate,
+      trackCount,
+    }: AlbumsType) => ({
+      artistId,
+      artistName,
+      collectionId,
+      collectionName,
+      collectionPrice,
+      artworkUrl100,
+      releaseDate,
+      trackCount,
+    }),
+  );
+  return response;
+}
--- src/types.ts
+++ src/types.ts
@@ -5,3 +5,14 @@ export const INITIAL_STATE = {
 export type CreateUserType = {
   name: string
 };
+
+export type AlbumsType = {
+  artistId: number
+  artistName: string
+  collectionId: number
+  collectionName: string
+  collectionPrice: number
+  artworkUrl100: string
+  releaseDate: string
+  trackCount: number
+};
