3f6de762782bce6716b0278ddc29cdb067302414
Diogo Sales <diogosales.ads@gmail.com>
2023-07-26 16:12:12 -0300

    feat: request to retrieve favorite songs and update the list after bookmarking a song

--- src/components/music-card/index.tsx
+++ src/components/music-card/index.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import addSong from '../../service/favoriteSongAPI';
+import { useEffect, useState } from 'react';
+import addSong, { getFavoriteSongs } from '../../service/favoriteSongAPI';
 import getMusic from '../../service/musicsAPI';
 import Loading from '../loading';
 import { AlbumType } from '../../types';
@@ -8,18 +8,29 @@ type MusicCardProps = {
   trackName: string
   previewUrl: string
   trackId: number | string
-  musicsFavorites: boolean
-
 };
 
-export default function MusicCard({
-  trackName, previewUrl, trackId, musicsFavorites }: MusicCardProps) {
+export default function MusicCard({ trackName, previewUrl, trackId }: MusicCardProps) {
   const [loanding, setLoading] = useState(false);
+  const [checked, setChecked] = useState(false);
+
+  function handleResult() {
+    const favoriteSongs: AlbumType[] = getFavoriteSongs();
+    setChecked(favoriteSongs.some((song) => song.trackId === trackId));
+  }
+
+  useEffect(() => {
+    setLoading(true);
+    handleResult();
+    setLoading(false);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   async function handleChange() {
     setLoading(true);
     const music: AlbumType = await getMusic(trackId.toString());
     addSong(music[0]);
+    handleResult();
     setLoading(false);
   }
 
@@ -39,7 +50,7 @@ export default function MusicCard({
           type="checkbox"
           id={ trackId.toString() }
           onChange={ handleChange }
-          checked={ musicsFavorites }
+          checked={ checked }
         />
       </label>
     </div>
