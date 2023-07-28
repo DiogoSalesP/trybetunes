0d9778e2549d864b6ba98911f3ef3c71eb06e437
Diogo Sales <diogosales.ads@gmail.com>
2023-07-28 17:35:37 -0300

    style: header component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,10 @@
 import { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { MagnifyingGlass, Star, UserCircle } from '@phosphor-icons/react';
 import { getUser } from '../../service/userAPI';
 import { CreateUserType } from '../../types';
+import logo from '../../images/logo.png';
+import './header.css';
 
 export default function Header() {
   const [user, setUser] = useState<CreateUserType>();
@@ -12,12 +15,25 @@ export default function Header() {
 
   return (
     <header>
-      <div>
-        <NavLink to="/search">Pesquisar</NavLink>
-        <NavLink to="/favorites">Músicas favoritas</NavLink>
-        <NavLink to="/profile">Perfil</NavLink>
+      <img src={ logo } alt="Logo" width={ 170 } height={ 95 } />
+      <div className="nav-link">
+        <NavLink to="/search">
+          <MagnifyingGlass size={ 21 } color="#444955" />
+          Pesquisa
+        </NavLink>
+        <NavLink to="/favorites">
+          <Star size={ 21 } color="#444955" />
+          Favoritas
+        </NavLink>
+        <NavLink to="/profile">
+          <UserCircle size={ 21 } color="#444955" />
+          Perfil
+        </NavLink>
       </div>
-      <p>{user?.name}</p>
+      <div className="img-p">
+        <img src="https://avatars.githubusercontent.com/u/102091772?v=4" alt="" width={ 150 } height={ 150 } />
+        <p>{user?.name}</p>
+      </div>
     </header>
   );
 }
