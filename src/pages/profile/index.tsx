9905801ea303fc2a73278132c956c6f05bf06ff2
Diogo Sales <diogosales.ads@gmail.com>
2023-08-04 17:39:41 -0300

    style: profile

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,13 +1,16 @@
+/* eslint-disable react/jsx-max-depth */
 import { useEffect, useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import Header from '../../components/header';
 import Loading from '../../components/loading';
 import { getUser } from '../../service/userAPI';
 import { CreateUserType } from '../../types';
+import './profile.css';
 
 export default function Profile() {
   const [loading, setLoading] = useState(false);
   const [user, setUser] = useState<CreateUserType>();
+  const navegate = useNavigate();
 
   useEffect(() => {
     setLoading(true);
@@ -15,31 +18,54 @@ export default function Profile() {
     setLoading(false);
   }, []);
 
+  useEffect(() => {
+    if (user?.email.length === 0) {
+      navegate('/profile/edit');
+    }
+  });
+
+  function handleClick() {
+    navegate('/profile/edit');
+  }
+
   if (loading || !user) {
     return <Loading />;
   }
   return (
-    <>
-      <Header />
-      <NavLink to="/profile/edit">Editar perfil</NavLink>
-      <div>
-        <p>
-          Nome:
-          {' '}
-          {user.name}
-        </p>
-        <p>
-          E-mail:
-          {' '}
-          {user.email}
-        </p>
-        <p>
-          Description:
-          {' '}
-          {user.description}
-        </p>
-        <img src={ user.image } alt={ `Foto de ${user.name}` } />
+    <div className="container-geral">
+      <div className="container-header">
+        <Header />
       </div>
-    </>
+      <div className="container-principal">
+        <div className="container-search" />
+        <div className="container-profile">
+          <img
+            className="image-profile"
+            src={ user.image }
+            alt={ `Foto de ${user.name}` }
+          />
+          <div className="user-profile">
+            <p>
+              <b>Nome:</b>
+              {' '}
+              {user.name}
+            </p>
+            <p>
+              <b>Email:</b>
+              {' '}
+              {user.email}
+            </p>
+            <p>
+              <b>Descrição:</b>
+              {' '}
+              {user.description}
+            </p>
+            <button onClick={ handleClick }>
+              Editar perfil
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
